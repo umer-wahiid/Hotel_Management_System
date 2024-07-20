9537fdd300987bdd0d7c72018e1f54ce08399c56
Umer Abdul Wahid <umerwahiid48@gmail.com>
2024-07-21 03:37:40 +0500

    CORS - Cross-Origin Resource Sharing

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,21 @@ import express, { response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import roomsRoute from './routes/roomsRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request);
