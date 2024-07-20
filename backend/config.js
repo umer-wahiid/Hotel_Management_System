c397a188ed07985d0e4ce26323ffaaa34db282d4
Umer Abdul Wahid & Ammar Ansari <umerwahiid48@gmail.com>
2024-07-20 16:18:51 +0500

    MongoDb Database Connected

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://umerwahiid48:dDI5YWdDWLkZ1CgW@hotelmanagementsystem.ffxaifm.mongodb.net/HotelManagementSystem?retryWrites=true&w=majority&appName=HotelManagementSystem'
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome To MERN')
+});
+
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App Is Listening To Port: ${PORT}`);
+    });
+})
+.catch((error)=>{
+    console.log(error)
+})
