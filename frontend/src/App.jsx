9af8ddb1b7d733d7abafc09203ca4dc6e885c097
Umer Abdul Wahid <umerwahiid48@gmail.com>
2024-07-21 15:08:53 +0500

    Tailwind & React-Router-Dom

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import CreateRoom from './pages/Room/CreateRoom'
+import DeleteRoom from './pages/Room/DeleteRoom'
+import EditRoom from './pages/Room/EditRoom'
+import ViewRoom from './pages/Room/ViewRoom'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/rooms/create' element={<CreateRoom />} />
+      <Route path='/rooms/details/:id' element={<ViewRoom />} />
+      <Route path='/rooms/edit/:id' element={<EditRoom />} />
+      <Route path='/rooms/delete/:id' element={<DeleteRoom />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Room/DeleteRoom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DeleteRoom = () => {
+  return (
+    <div>DeleteRoom</div>
+  )
+}
+
+export default DeleteRoom
